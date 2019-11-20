c96f2a91abfea65e746cf22d268c9c3d244cd31a
Ben Blamey <blamey.ben@gmail.com>
2019-11-20 17:41:53 +0100

    first results

--- readme.md
+++ readme.md
@@ -44,18 +44,39 @@ http://localhost:4040/api/v1/applications
 http://localhost:4040/api/v1/applications/app-20191119153056-0033/executors
     > lists number of cores for each executor
 
-2. start shuffle service (required for autoscaling)
 
 3. enable autoscaling 
 
-4. bash script:
-    for loop > curl > python (parse json, compute total cores) > stdout (with time)
-    
+TODO: scaling policy in Spark -- by default it will try to 'spread out' the executors (to maximize IO throughput, this is the opposite of what HIO does)
+
+
+
+profiling:
+```
+
+# profile CPU (run this on each worker)
+rm cpu.log ; while : ; do  echo "$(top -b -n 1 | grep Cpu) -  $(($(date +%s%N)/1000000))mS since epoch"; sleep 0.5; done >> cpu.log
+
+
+
+# Poll the total number of cores (just do this on the master)
+# Need to first get AppID from eg. http://localhost:4040/api/v1/applications
+rm cores.log ; while : ; do  echo "$(curl -s http://localhost:4040/api/v1/applications/app-20191120193218-0005/executors | jq 'reduce .[] as $item (0; . + $item.totalCores)') -  $(($(date +%s%N)/1000000))mS since epoch"; sleep 1; done >> cores.log
+```
+
+
+rsync 130.238.28.97:~/*.log spark/data
     
 
 # Copy 1 file
-cd /mnt/images/Salman_Cell_profiler_data/Data ; cp Nuclear\ images/011001-1-001001001.tif src/
+rm src/* ; cd /mnt/images/Salman_Cell_profiler_data/Data ; cp Nuclear\ images/011001-1-001001001.tif src/
 
 # Copy all files
-cd /mnt/images/Salman_Cell_profiler_data/Data ; cp Nuclear\ images/* src/
+rm src/* ; cd /mnt/images/Salman_Cell_profiler_data/Data ; cp Nuclear\ images/* src/
 
+
+# Copy all files with a small pause (this helps spark to create smaller batches - so it can scale in a timely fashion
+rm src/* ; find ./Nuclear\ images -type f | xargs -I file sh -c "(cp \"file\" ./src; sleep 0.1)"
+
+# copy 20 images
+rm src/* ; find ./Nuclear\ images -type f | head -n 20 | xargs -I file sh -c "(cp \"file\" ./src; sleep 0.1)"
--- spark/spark-scala-cellprofiler/src/main/scala/CellProfilerStreaming.scala
+++ spark/spark-scala-cellprofiler/src/main/scala/CellProfilerStreaming.scala
@@ -11,7 +11,7 @@ object CellProfilerStreaming {
 
   val logger = Logger.getRootLogger()
 
-  val BATCH_INTERVAL = 5
+  val BATCH_INTERVAL = 5 //5
   val SPARK_MASTER_URL: String = "spark://192.168.1.15:7077"
   val PATH: String = "/mnt/images/Salman_Cell_profiler_data/Data/src/"
 
@@ -53,7 +53,38 @@ object CellProfilerStreaming {
     val sparkSession = SparkSession.builder
       .master(SPARK_MASTER_URL)
       .appName("CellProfiler")
+
+
+
+      // Implemented with: https://issues.apache.org/jira/browse/SPARK-12133
+      // See: https://medium.com/@pmatpadi/spark-streaming-dynamic-scaling-and-backpressure-in-action-6ebdbc782a69
+      // These don't seem to be documented officially? (yet?)
+      //      .config("spark.streaming.dynamicAllocation.enabled", true)
+      //      .config("spark.streaming.dynamicAllocation.scalingInterval", 10)
+      //      .config("spark.streaming.dynamicAllocation.minExecutors", 1)
+
+
+      // Scaling up is driven by ratio of batch interval to processing time.
+      // But, it doesn't apply until an entire batch is copied. So, we want a pause between copying the files.
+      // And we also use a short batch interval (of 1 second). this way, we complete the first batch within a few seconds, so
+      // we can begin the scaling.
+
+      // .. this doesn't really work -- because the jobs are processed sequentially, so we can never use all of the cores properly.
+      // since we must have a small number of files in the batch interval in order for timely scale up,
+      // but enough files in the batch interval to use all the cores when it has scaled up.
+
+      // So instead, try with the traditional mechanism.
+      .config("spark.shuffle.service.enabled", true)
+      .config("spark.dynamicAllocation.enabled", true)
+      .config("spark.dynamicAllocation.executorIdleTimeout", 10)
+
+
       .getOrCreate()
+
+    //    NOTE: scaling in spark is based on executors. by default, one executor per node (with all cores)
+    //    So, we instead set 1 core/executor, to get finer granularity. pay some overhead for this.
+    // We configure this in /conf on the master
+    // TODO: does this setting also work if set from here? should do, the application owns the executors.
 
     sparkSession.sparkContext.setLogLevel("INFO")
 
